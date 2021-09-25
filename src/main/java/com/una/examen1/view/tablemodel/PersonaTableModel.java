2075814412882f9a6b2258e2e83937fcee6e271a
Jonathan Guzmán <jonguz5@hotmail.com>
2021-09-24 23:46:52 -0600

    agregados TableModel

---
+++ src/main/java/com/una/examen1/view/tablemodel/PersonaTableModel.java
@@ -0,0 +1,80 @@
+package com.una.examen1.view.tablemodel;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+
+import com.una.examen1.model.Persona;
+
+class PersonaTableModel extends AbstractTableModel {
+
+    public PersonaTableModel() {
+        this(new ArrayList<Persona>());
+    }
+
+    public PersonaTableModel(List<Persona> personas) {
+        this.personas = personas;
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        suscriptores.add(l);
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return String.class;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLUMNAS.length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return COLUMNAS[columnIndex];
+    }
+
+    @Override
+    public int getRowCount() {
+        return personas.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Persona p = personas.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return String.format("%-2d", p.getID());
+            case 1:
+                return String.format("%s", p.getNombre());
+            case 2:
+                return String.format("%-2d", p.getCantidad());
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        suscriptores.remove(l);
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        // ningun campo se puede editar
+    }
+
+    private List<Persona> personas;
+    private LinkedList<TableModelListener> suscriptores = new LinkedList<>();
+    private final static String[] COLUMNAS = { "Id", "Nombre", "Premios" };
+}
