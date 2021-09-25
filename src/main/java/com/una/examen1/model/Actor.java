fc10d927099318d3fc7b6fe0c4a2c51367cd0c34
Jonathan Guzmán <jonguz5@hotmail.com>
2021-09-25 14:10:48 -0600

    marshall actor

--- src/main/java/com/una/examen1/model/Actor.java
+++ src/main/java/com/una/examen1/model/Actor.java
@@ -1,5 +1,11 @@
 package com.una.examen1.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Actor extends Persona {
     public Actor(Integer id, String nombre, Integer cantidad) {
         super(id, nombre, cantidad);
