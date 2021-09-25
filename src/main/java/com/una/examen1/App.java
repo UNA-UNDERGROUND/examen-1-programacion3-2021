e193f191253fd50bf775785c91bfaa35a4b3d38d
Jonathan Guzmán <jonguz5@hotmail.com>
2021-09-24 22:34:24 -0600

    agregadas vistas

--- src/main/java/com/una/examen1/App.java
+++ src/main/java/com/una/examen1/App.java
@@ -1,7 +1,24 @@
 package com.una.examen1;
 
+import com.una.examen1.view.MenuPrincipal;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class App {
-    public static void main(String[] args) {
-        
+
+    static void mostrarUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) {
+            System.err.println(ex.getLocalizedMessage());
+        }
+        java.awt.EventQueue.invokeLater(() -> {
+            new MenuPrincipal().setVisible(true);
+        });
     }
+
+    public static void main(String[] args) {
+        mostrarUI();
+    }
+
 }
