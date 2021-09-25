5e3130e2c2e43b731a9e773a55c0f0c6f88b70c8
Jonathan Guzmán <jonguz5@hotmail.com>
2021-09-25 00:24:18 -0600

    singleton controlador

--- src/main/java/com/una/examen1/controller/ControladorCinema.java
+++ src/main/java/com/una/examen1/controller/ControladorCinema.java
@@ -1,5 +1,29 @@
 package com.una.examen1.controller;
 
+import java.util.ArrayList;
+
+import com.una.examen1.model.Actor;
+import com.una.examen1.model.Director;
+import com.una.examen1.model.Pelicula;
+
 public class ControladorCinema {
-    
+
+    private ControladorCinema(){
+
+    }
+
+
+
+    private ArrayList<Director> directores;
+    private ArrayList<Actor> actores;
+    private ArrayList<Pelicula> peliculas;
+
+    private static ControladorCinema instancia = null;
+
+    public static ControladorCinema getInstancia(){
+        if(instancia == null){
+            instancia = new ControladorCinema();
+        }
+        return instancia;
+    }
 }
